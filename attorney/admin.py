5a2437534bb0ea93fe814427d62a41cda5823016
alam0164088 <alam01640882530@gmail.com>
2026-02-10 02:10:10 +0600

    u

--- attorney/admin.py
+++ attorney/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ConsultationRequest, Message, Event, AttorneyRating
 
 # Register your models here.
+admin.site.register(ConsultationRequest)
+admin.site.register(Message)
+admin.site.register(Event)
+admin.site.register(AttorneyRating)
